118be313ebfdefd8681eccfa92df513aa26fc066
bhavesh.modi <bhavesh.modi@condecosoftware.com>
2025-07-03 14:13:39 +0530

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+	public class TestController : Controller
+	{
+		public IActionResult Index()
+		{
+			var data = new TestViewModel()
+            {
+				Name = "Student of MVC Master",
+				DateOfBirth = new DateTime(1955,08,11)
+            };
+
+			return View(data);
+		}
+	}
+}
